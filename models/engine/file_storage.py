a4e20f97c0d1fa83b12bb26199697a08e61ffe25
TeddyMuli <tmuli974@gmail.com>
2023-05-16 17:29:24 +0000

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,7 +35,7 @@ class FileStorage:
         """
         sets in __objects the `obj` with key <obj class name>.id
         """
-        obj = self.__objects[f"{obj.__class__.__name__}.{obj.id}"]
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """
